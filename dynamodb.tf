09fdc2276da84b214b6a19c77812d3dbec5e30e4
Juan - Ptwo <jcrestrepobedoya@gmail.com>
2020-09-07 22:53:18 -0500

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -4,9 +4,9 @@ resource "aws_dynamodb_table" "ddb_table" {
   write_capacity         = "${var.ddb-write-capacity}"
   hash_key               = "${var.ddb-hash-key-name}"
   range_key              = "${var.ddb-range-key-name}"
-  attribute              = ["${local.attributes_final}"]
-  local_secondary_index  = ["${var.lsi-list}"]
-  global_secondary_index = ["${var.gsi-list}"]
+  //attribute              = ["${local.attributes_final}"]
+  //local_secondary_index  = ["${var.lsi-list}"]
+  //global_secondary_index = ["${var.gsi-list}"]
   tags                   = "${var.tags}"
   stream_enabled         = "${var.stream-enabled}"
   stream_view_type       = "${var.stream-view-type}"
