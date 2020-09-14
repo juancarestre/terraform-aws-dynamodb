0c139cbdb055fefeada5b173b60fc3b587966125
Juan - Ptwo <jcrestrepobedoya@gmail.com>
2020-09-13 20:27:10 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "dynamodb-arn" {
   description = "The ARN of the DynamoDB."
   value       = "${aws_dynamodb_table.ddb_table.arn}"
 }
+
+output "table_name" {
+  description = "The ARN of the DynamoDB."
+  value       = "${aws_dynamodb_table.ddb_table.id}"
+}
